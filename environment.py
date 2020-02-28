6ea2b6c0bfdf16ae6c49f39879cfb59b9b4db5e6
root <root@mongodb-test.asia-northeast3-a.c.valid-arc-269304.internal>
2020-02-28 13:32:11 +0000

    test

---
+++ environment.py
@@ -0,0 +1,5 @@
+class StockTradingEnvironment(gym.Env):
+    def __init__(self, n_items):
+        super().__init__()
+        self.action_space = Simplex(shape=(n_items,))
+        # self.observation_space =
