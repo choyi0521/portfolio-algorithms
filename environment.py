b108c5b8f20a30a13f16469749953b038c332b36
root <choyi0521@gmail.com>
2020-02-29 10:04:53 +0000

    test

--- environment.py
+++ environment.py
@@ -1,8 +1,11 @@
+import gym
+from ray.rllib.models.extra_spaces import Simplex
+
 class StockTradingEnvironment(gym.Env):
     def __init__(self, n_items):
         super().__init__()
         self.action_space = Simplex(shape=(n_items,))
         # self.observation_space =
 
-ste = StockTradingEnvironment()
-        
+ste = StockTradingEnvironment(3)
+print(ste.action_space.sample())
